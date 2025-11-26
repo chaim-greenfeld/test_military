8d4dfcc28f29e7afb65be820e3112d74648e0aa5
yossi <ix44248@gmail.com>
2025-11-26 17:48:46 +0200

    finish test

--- military_base.py
+++ military_base.py
@@ -8,8 +8,57 @@ import io
 
 app = FastAPI()
 
+class Soldier:
+    def __init__(self, personal_number,f_name, l_name, gender, city, residential_distance):
+        self.personal_number = personal_number
+        self.f_name = f_name
+        self.l_name = l_name
+        self.gender = gender
+        self.city = city
+        self.residential_distance = residential_distance
+        self.placement_status = f"{False}, in waiting list"
 
 
+
+class Room:
+    
+    def __init__(self):
+        
+        
+        self.soldier = []
+        self.is_full = False
+
+    def add_mens(self,  militery_mens):
+        self.soldier.append(militery_mens)
+        militery_mens.placement_status = True
+        
+        if len(self.soldier) == 8:
+            self.is_full = True
+
+            return "There is no room."     
+        return f"there is {len(self.soldier)} soldier in room"
+    
+class Home_mili:
+    def __init__(self):
+        self.rooms:list[Room]=[Room() for _ in range(10)]
+
+
+drop_a = Home_mili()
+drop_b = Home_mili()
+class Base:
+    drop = [drop_a, drop_b]
+
+
+
+
+
+
+
+
+
+
+
+soldiers_sorti=[]
 def import_military_men_from_csv(csv_content: bytes) -> dict:
     csv_text = csv_content.decode('utf-8').splitlines()
     params=[]
@@ -30,5 +79,22 @@ async def upload_car_owners_csv(file: UploadFile = File(...)):
     contents = await file.read()
 
     result = import_military_men_from_csv(contents)
-    
-    return result
+    result.sort(key = lambda x: int(x['מרחק מהבסיס']))
+
+    for i in result:
+        i['מספר אישי'] = Soldier(i['מספר אישי'], i['שם פרטי'], i['שם משפחה'], i['מין'], i['עיר מגורים'], i['מרחק מהבסיס'], )
+        soldiers_sorti.append(i['מספר אישי'])
+    result = soldiers_sorti.copy()
+    for i in Base.drop:
+        for j in i.rooms:
+            while not j.is_full:
+                j.add_mens(soldiers_sorti.pop())
+
+    return {"Soldiers who were deployed":len(result) - len(soldiers_sorti), "Soldiers on the waiting list": len(soldiers_sorti)}, result
+
+
+
+
+
+
+
